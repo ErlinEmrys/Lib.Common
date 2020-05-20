6508434a4dce965cf4c0bac9e0ab01d895f9f60b
Erlin Emrys <Erlin@seznam.cz>
2020-05-20 09:43:38 +0200

    AssemblyHelper.BaseLocation fix -PublishSingleFile option

--- Erlin.Lib.Common/Reflection/AssemblyHelper.cs
+++ Erlin.Lib.Common/Reflection/AssemblyHelper.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+
+using Erlin.Lib.Common.FileSystem;
 
 namespace Erlin.Lib.Common.Reflection
 {
@@ -13,6 +16,8 @@ namespace Erlin.Lib.Common.Reflection
     /// </summary>
     public static class AssemblyHelper
     {
+        private static string? _baseLocation;
+
         /// <summary>
         /// Return current Lib.Common assembly
         /// </summary>
@@ -21,6 +26,28 @@ namespace Erlin.Lib.Common.Reflection
         /// <summary>
         /// Path to location of this base assembly
         /// </summary>
-        public static string BaseLocation { get; } = Path.GetDirectoryName(CommonBaseAssembly.Location) ?? throw new InvalidOperationException();
+        public static string BaseLocation
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_baseLocation))
+                {
+                    string? dir = null;
+                    Process currentProcess = Process.GetCurrentProcess();
+                    if (currentProcess.MainModule != null)
+                    {
+                        dir = FileHelper.GetDirectoryPath(currentProcess.MainModule.FileName);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Current process missing MainModule!");
+                    }
+
+                    _baseLocation = dir;
+                }
+
+                return _baseLocation;
+            }
+        }
     }
 }
