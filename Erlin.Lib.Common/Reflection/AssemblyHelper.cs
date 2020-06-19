f48baaf6eabbd3bd3b8cf6e9f5eb264fb464200d
Erlin Emrys <Erlin@seznam.cz>
2020-06-19 16:25:47 +0200

    Revert "AssemblyHelper.BaseLocation fix -PublishSingleFile option"
    This reverts commit 6508434a4dce965cf4c0bac9e0ab01d895f9f60b.

--- Erlin.Lib.Common/Reflection/AssemblyHelper.cs
+++ Erlin.Lib.Common/Reflection/AssemblyHelper.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-
-using Erlin.Lib.Common.FileSystem;
 
 namespace Erlin.Lib.Common.Reflection
 {
@@ -16,8 +13,6 @@ namespace Erlin.Lib.Common.Reflection
     /// </summary>
     public static class AssemblyHelper
     {
-        private static string? _baseLocation;
-
         /// <summary>
         /// Return current Lib.Common assembly
         /// </summary>
@@ -26,28 +21,6 @@ namespace Erlin.Lib.Common.Reflection
         /// <summary>
         /// Path to location of this base assembly
         /// </summary>
-        public static string BaseLocation
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(_baseLocation))
-                {
-                    string? dir = null;
-                    Process currentProcess = Process.GetCurrentProcess();
-                    if (currentProcess.MainModule != null)
-                    {
-                        dir = FileHelper.GetDirectoryPath(currentProcess.MainModule.FileName);
-                    }
-                    else
-                    {
-                        throw new InvalidOperationException("Current process missing MainModule!");
-                    }
-
-                    _baseLocation = dir;
-                }
-
-                return _baseLocation;
-            }
-        }
+        public static string BaseLocation { get; } = Path.GetDirectoryName(CommonBaseAssembly.Location) ?? throw new InvalidOperationException();
     }
 }
